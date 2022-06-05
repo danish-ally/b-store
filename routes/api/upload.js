b54d4e97304d30060cb0f339a828f16228a283ed
danish-ally <dannush786@gmail.com>
2022-06-05 11:59:04 +0530

    chnges in api

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -76,83 +76,80 @@ router.post("/store/:id", upload.array("shopImage"), async (req, res) => {
   }
 });
 
-router.post(
-  "/category/:id",
-  upload.array("categoryImage"),
-  async (req, res) => {
-    const category = await Category.findById(req.params.id);
+router.post("/category/:id", upload.array("file"), async (req, res) => {
+  const category = await Category.findById(req.params.id);
 
-    try {
-      if (req.files.length < 2) {
-        return res.status(400).json({
-          // in case things don't work out
-          msg: "Please upload minimum 2 images",
-        });
-      }
-
-      console.log("first");
-      if (req.files) {
-        // if you are adding multiple files at a go
-        console.log("fff");
-        const imageURIs = []; // array to hold the image urls
-        const imageIds = [];
-        const files = req.files; // array of images
-        // console.log(files);
-        for (const file of files) {
-          // console.log(file);/
-          const result = await cloudinary.uploader.upload(file.path);
-          console.log(file.path);
-          imageURIs.push(result.secure_url);
-          imageIds.push(result.public_id);
-        }
-
-        category["categoryImage"] = imageURIs; // add the urls to object
-        category["categoryImageId"] = imageIds;
-
-        const c1 = await category.save();
-        return res.status(200).json({
-          success: true,
-          message: `images successfully uploaded!`,
-          category: c1,
-        });
-      }
-
-      // if (req.file && req.file.path) {
-      //   // if only one image uploaded
-      //   store["shopImage"] = req.file.path; // add the single
-      //   const s1 = await store.save();
-      //   return res.status(200).json({
-      //     success: true,
-      //     message: `images successfully uploaded!`,
-      //     store: s1,
-      //   });
-      // }
-
-      // you could save here without the image
-
+  console.log(req.file);
+  try {
+    if (req.files.length < 1) {
       return res.status(400).json({
         // in case things don't work out
-        msg: "Please upload an image",
+        msg: "Please upload minimum 1 images",
       });
-    } catch (err) {
-      if (err) {
-        console.log(err);
-        return res.status(400).json({
-          error: "Your request could not be processed. Please try again.....",
-        });
+    }
+
+    console.log("first");
+    if (req.files) {
+      // if you are adding multiple files at a go
+      console.log("fff");
+      const imageURIs = []; // array to hold the image urls
+      const imageIds = [];
+      const files = req.files; // array of images
+      // console.log(files);
+      for (const file of files) {
+        // console.log(file);/
+        const result = await cloudinary.uploader.upload(file.path);
+        console.log(file.path);
+        imageURIs.push(result.secure_url);
+        imageIds.push(result.public_id);
       }
+
+      category["categoryImage"] = imageURIs; // add the urls to object
+      category["categoryImageId"] = imageIds;
+
+      const c1 = await category.save();
+      return res.status(200).json({
+        success: true,
+        message: `images successfully uploaded!`,
+        category: c1,
+      });
+    }
+
+    // if (req.file && req.file.path) {
+    //   // if only one image uploaded
+    //   store["shopImage"] = req.file.path; // add the single
+    //   const s1 = await store.save();
+    //   return res.status(200).json({
+    //     success: true,
+    //     message: `images successfully uploaded!`,
+    //     store: s1,
+    //   });
+    // }
+
+    // you could save here without the image
+
+    return res.status(400).json({
+      // in case things don't work out
+      msg: "Please upload an image",
+    });
+  } catch (err) {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: "Your request could not be processed. Please try again.....",
+      });
     }
   }
-);
+});
 
-router.post("/product/:id", upload.array("productImage"), async (req, res) => {
+router.post("/product/:id", upload.array("file"), async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   try {
-    if (req.files.length < 3) {
+    if (req.files.length < 1) {
       return res.status(400).json({
         // in case things don't work out
-        msg: "Please upload minimum 3 images",
+        msg: "Please upload minimum 1 images",
       });
     }
 
